10114be86d7fbf2bc7ed7d347b18fbd0e945945b
Aryyaa9 <153473157+Aryyaa9@users.noreply.github.com>
2023-12-22 18:37:59 +0700

    Add files via upload

---
+++ fifth/fifth/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace ConsoleApplication3
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            int[,] matrix = new int[n, m + 1];
+
+            Random random = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matrix[i, j] = random.Next(0, 2);
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            int count = 0; //для подсчета единиц в каждой строке
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (matrix[i, j] == 1)
+                        count++;
+                }
+                
+                // для создания еще 1го столбца 
+                if (count % 2 == 0)
+                    matrix[i, m] = 0;
+                else
+                    matrix[i, m] = 1;
+                count = 0;
+            }
+            Console.WriteLine();
+
+            
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= m; j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ fourth/fourth/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApplication3
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine()); // положит-ое число
+            int num = 0;
+            int sum = 0;
+
+            while (num >= 0)
+            {
+                num = int.Parse(Console.ReadLine());
+                if (num % a == 0)
+                {
+                    sum += num;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
